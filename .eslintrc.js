94c2121e3f940cf57cbf64833f83a0c256f2e2ce
gerarmc <gerardomelendezpro@gmail.com>
2025-03-15 16:43:50 -0400

    working registration issues gerardomarch152025

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier'
   ],
   root: true,
   env: {
@@ -25,5 +26,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
   },
 };
